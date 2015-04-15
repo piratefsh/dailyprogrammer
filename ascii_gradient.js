cbc68a76e7905d7df3de720d3de983f917f1bdf4
Sher Minn Chong <sherminn.chong@softspace.com.my>
2015-04-15 18:03:51 +0800

    Somewhat working

--- ascii_gradient.js
+++ ascii_gradient.js
@@ -1,4 +1,4 @@
-var x = parseInt(process.argv[2]), y = parseInt(process.argv[3]), gradientArgs = process.argv[4].split(',');
+var x = parseInt(process.argv[3]), y = parseInt(process.argv[2]), gradientArgs = process.argv[4].split(',');
 var type = gradientArgs[0], args;
 if(type == "radial"){
     args = {
@@ -11,21 +11,20 @@ if(type == "radial"){
 }
 else if(type=="linear"){
     args = {
-        startX: gradientArgs[1],
-        startY: gradientArgs[2],
-        endX: gradientArgs[3],
-        endY: gradientArgs[4],
+        startX: gradientArgs[2],
+        startY: gradientArgs[1],
+        endX: gradientArgs[4],
+        endY: gradientArgs[3],
         colors: ['.', ',', ':', ';', 'x', 'X', '&', '@'],
         type: type
     }
 }
-
 function printGrid(grid){
     for(var i = 0; i < grid.length; i++){
         for(var j = 0; j < grid[i].length; j++){
             var color = grid[i][j] || '-';
 
-            process.stdout.write(' ' + color);
+            process.stdout.write('' + color);
         }
         process.stdout.write('\n');
     }
@@ -41,7 +40,7 @@ function makeGradient(grid, args){
 }
 
 function getColor(colors, distance, maxDistance){
-    if(distance > maxDistance || distance < 0) {
+    if(distance > maxDistance) {
         return null;
     }
     var index = Math.floor(distance / maxDistance * colors.length);
@@ -49,29 +48,40 @@ function getColor(colors, distance, maxDistance){
 }
 
 function findDistance(x1, y1, x2, y2){
+    return Math.floor(findRawDistance(x1, y1, x2, y2));
+}
+
+function findRawDistance(x1, y1, x2, y2){
     var x = Math.pow(x2 - x1, 2);
     var y = Math.pow(y2 - y1, 2);
-    return Math.floor(Math.pow((x + y), 0.5));
+    return Math.sqrt(x + y);
 }
 
 function findLinearDistance(x, y, x1, y1, x2, y2){
     var constX, constY, constC, slope;
-    slope = 1.0 * (y2 - y1) / (x2 - x1);
+    slope = (1.0 * (y2 - y1)) / (x2 - x1);
     constY = 1.0;
     constX = -1.0 * slope;
-    constC = y1 - slope*x1;
-    var perpendicularDistance = Math.abs(constX * x + constY * y + constC) / Math.pow(Math.pow(constX, 2) + Math.pow(constY/2),0.5);
-    // return Math.floor(Math.abs((x1-x)/2 + (y1-y)/2));
-    return perpendicularDistance; 
+    constC = slope * x1 - y1;
+    var pTop = Math.abs(constX * x + constY * y + constC);
+    var pBottom = Math.sqrt(Math.pow(constX, 2) + Math.pow(constY, 2));
+    var perpendicularDistance = (1.0 * pTop) / pBottom; 
+
+    var a, b, c; //pythagorean
+    a = perpendicularDistance;
+    c = findRawDistance(x, y, x1, y1);
+    
+    b = Math.sqrt(Math.pow(c,2) - Math.pow(a,2));
+    return Math.floor(b); 
 }
 
 function makeLinearGradient(grid, args){
-    var maxDistance = Math.abs(findLinearDistance(args.startX, args.startY, args.endX, args.endY));
+    var maxDistance = findDistance(args.startX, args.startY, args.endX, args.endY)/2;
+    console.log(maxDistance)
     for(var i = 0; i < grid.length; i++){
         for(var j = 0; j < grid[i].length; j++){
-            var d = findLinearDistance(i, j, args.startX, args.startY, args.endX, args.endY);
+            var d = findLinearDistance(i, j, args.endX, args.endY, args.startX, args.startY);
             var c = getColor(args.colors, d, maxDistance);
-            console.log(d + ', ' + maxDistance)
             grid[i][j] = c;
         }
     }
