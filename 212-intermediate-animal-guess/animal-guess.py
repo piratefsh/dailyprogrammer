7084e70c329c7a1940baf4be2118c28bea78f621
Sher Minn Chong <sherminn.chong@softspace.com.my>
2015-05-02 10:41:38 +0800

    Implement loading

--- 212-intermediate-animal-guess/animal-guess.py
+++ 212-intermediate-animal-guess/animal-guess.py
@@ -1,3 +1,5 @@
+savefile = 'animals.txt'
+
 # Question tree node
 class Node:
     def __init__(self, content):
@@ -12,7 +14,7 @@ class Node:
         return self.yes or self.no
 
 # returns tree as a string recursively
-def tree_to_string(root, count):
+def tree_to_string(root, count=0):
     curr = root
     if not curr:
         return ""
@@ -25,53 +27,76 @@ def tree_to_string(root, count):
         padding += "-"
     return padding + curr.content + left + right
 
-def save(content):
-    f = open('animals.txt', 'w')
-    f.write(content)
+def tree_as_array(curr):
+    if not curr:
+        return []
+    arr = [curr] + tree_as_array(curr.yes) + tree_as_array(curr.no)
+    return arr
 
-# initialize tree with single question and animal
-init_leaf   = Node('cat')
-root        = Node('Are you thinking of an animal? ')
-root.yes    = init_leaf
+def array_to_tree(arr, index=0):
+    if index >= len(arr):
+        return None
+    tree = Node(arr[index])
+    tree.yes = array_to_tree(arr, (index+1)*2-1)
+    tree.no = array_to_tree(arr, (index+1)*2)
+    return tree
 
-while True:
-    # start from root node, travel down tree
-    curr        = root
+def load(file):
+    f = open(savefile, 'r')
+    arr = f.readlines()
+    tree = array_to_tree(arr)
+    return tree
+
+def save(arr):
+    f = open(savefile, 'w')
+    for node in arr:
+        line = node.content if node else ""
+        f.write(line.strip() + "\n")
+    f.close()
+
+def start():
+    root = load(savefile)
+    print(tree_to_string(root))
     while True:
-        prev     = curr
-        response = input(curr.content)
-        curr     = curr.getNext(response)
-        if not curr.getNext(response):
-            break
-     
-    prev_response = response 
+        # start from root node, travel down tree
+        curr        = root
+        while True:
+            prev     = curr
+            response = input(curr.content)
+            curr     = curr.getNext(response)
+            if not curr.getNext(response):
+                break
+         
+        prev_response = response 
 
-    # check if we guessed animal correctly  
-    guessed_animal = curr.content
-    response = input('Are you thinking of a %s? ' % guessed_animal)
+        # check if we guessed animal correctly  
+        guessed_animal = curr.content
+        response = input('Are you thinking of a %s? ' % guessed_animal)
 
-    # guessed right
-    if response is 'y':             
-        print('Thanks for playing!')
+        # guessed right
+        if response is 'y':             
+            print('Thanks for playing!')
 
-    # guessed wrongly
-    else:                           
-        new_animal      = input('Oh dear, what was the right answer? ')
-        new_question    = input('What is a question that distinguishes %s from %s? ' % (new_animal, guessed_animal))
-        new_animal_response = input('What would the answer be for %s? ' % new_animal)
+        # guessed wrongly
+        else:                           
+            new_animal      = input('Oh dear, what was the right answer? ')
+            new_question    = input('What is a question that distinguishes %s from %s? ' % (new_animal, guessed_animal))
+            new_animal_response = input('What would the answer be for %s? ' % new_animal)
 
-        # add new question
-        new_node = Node(new_question + " ")
-        if new_animal_response is 'y':
-            new_node.yes    = Node(new_animal)
-            new_node.no     = curr
-        else:
-            new_node.no     = Node(new_animal)
-            new_node.yes    = curr
+            # add new question
+            new_node = Node(new_question + " ")
+            if new_animal_response is 'y':
+                new_node.yes    = Node(new_animal)
+                new_node.no     = curr
+            else:
+                new_node.no     = Node(new_animal)
+                new_node.yes    = curr
 
-        if prev_response is 'y':
-            prev.yes    = new_node
-        else:
-            prev.no     = new_node
+            if prev_response is 'y':
+                prev.yes    = new_node
+            else:
+                prev.no     = new_node
 
-    save(tree_to_string(root, 0))
+        save(tree_as_array(root))
+
+start()
