4a61bc89cd6aa9cfa86e058c41ad06c64b574e4e
Sher Minn Chong <sherminnchong@Sher-Minn-MBP.local>
2015-05-27 17:10:42 +0200

    Update

--- 215-validating-sorting-networks/validate-network.py
+++ 215-validating-sorting-networks/validate-network.py
@@ -3,13 +3,10 @@ infile 		= open(sys.argv[1])
 lines 		= infile.read().split('\n')
 comparators = [[int(i) for i in c.split()] for c in lines[1:]]
 wires 		= int(lines[0].split()[0])
-tests   	= list(itertools.product('01', repeat=wires))
-
+tests		= itertools.product([0,1], repeat=wires)
 for test in tests:
 	test = list(test)
-	for comparison in comparators:
-		left = comparison[0]
-		right = comparison[1]
+	for [left, right] in comparators:
 		if test[left] > test[right]:
 			test[left], test[right] = test[right], test[left]
 	if sorted(test) != test:
