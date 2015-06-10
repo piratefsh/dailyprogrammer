713fa213bca356d186ce378188db6301fb0566cf
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-10 09:38:45 +0800

    Simplufy palindrom check

--- 218-easy-making-numbers-palindromic/palindromic-numbers.py
+++ 218-easy-making-numbers-palindromic/palindromic-numbers.py
@@ -1,15 +1,10 @@
 import sys
 
-def is_palindrome(s):
-	if len(s) < 2:
-		return True
-	return s[0] == s[-1] and is_palindrome(s[1:-1])
-
 input_num 	= sys.argv[1]
 num 		= input_num
 steps 		= 0
 
-while not is_palindrome(num):
+while not num == num[::-1]:
 	steps += 1
 	num = str(int(num) + int(num[::-1]))
 
