c7b3892e84379b729c1602f719b031d076bc67e5
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-14 15:35:03 +0800

    Refactor

--- 218-intermediate-generating-polyominoes/generate_polyominoes.py
+++ 218-intermediate-generating-polyominoes/generate_polyominoes.py
@@ -6,11 +6,19 @@ size = int(sys.argv[1])
 center = size/2-1
 init = Polyomino(size).set((center, center))
 
+def exists(this, polys):
+	for p in polys:
+		if this == p:
+			return True
+	return False
+
 def find_polyominoes(poly, tiles_left, polys):
 	# if no more tiles left to add, return
 	if tiles_left == 1:
 		# add to array
-		polys.append(poly.normalized())
+		norm = poly.normalized()
+		if not exists(norm, polys):
+			polys.append(norm)
 		return
 	# else, if still have tiles
 	else:
@@ -23,16 +31,10 @@ def find_polyominoes(poly, tiles_left, polys):
 			p.set(side)
 			# find more polyominoes with new polyomino and add those to array
 			find_polyominoes(p, tiles_left, polys)
-
 polys = []
 find_polyominoes(init, size, polys)
 
-cleared = []
 for p in polys:
-	if p not in cleared:
-		cleared.append(p)
+	print(p)
+print(len(polys))
 
-for c in cleared:
-	print c
-
-print(len(cleared))
