57f5fb973b0cb8e9c973e1c8ae2d8a43ea78e17f
Sher Minn Chong <sherminn.chong@gmail.com>
2015-06-23 16:15:10 +0800

    Implement eating and waiting

--- misc/dining_philosophers.py
+++ misc/dining_philosophers.py
@@ -1,4 +1,11 @@
-class Philosopher():
+import time
+import logging
+from threading import Thread
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] (%(threadName)-10s) %(message)s',)
+logging.getLogger().setLevel(logging.INFO)
+
+
+class Philosopher(Thread):
 	"""
 	Philosopher instance runs in their own thread. 
 	A Philosopher does the following indefinitely:
@@ -9,10 +16,53 @@ class Philosopher():
 	"""
 
 	counter = 0
+	eat_time = 1
+	think_time = 0.5
 
 	def __init__(self):
+		Thread.__init__(self)
 		self.id = Philosopher.counter
+		self.setName('Philosopher %d' % self.id)
 		Philosopher.counter += 1
+
+
+	def run(self):
+		""" 
+		Think and eat forever
+		"""
+		for _ in range(10):
+			self.get_forks()
+			self.eat()
+			self.release_forks()
+			self.think()
+
+	def get_forks(self):
+		"""
+		Get forks on right and left of Philosopher if available
+		"""
+		return 
+
+	def eat(self):
+		"""
+		Spend time eating
+		"""
+		logging.info(repr(self) + " is eating")
+		time.sleep(self.eat_time)
+		return 
+
+	def release_forks(self):
+		"""
+		Return forks to their original places
+		"""
+		return 
+
+	def think(self):
+		"""
+		Spend time thinking
+		"""
+		logging.info(repr(self) + " is thinking")
+		time.sleep(self.think_time)
+		return
 
 	def __repr__(self):
 		return "Philosopher %d" % self.id
@@ -28,4 +78,8 @@ class Table():
 	Table contains 5 Philosophers and 5 forks between them
 	"""
 
-	print [Philosopher(), Philosopher(), Philosopher()]
+def test():
+	philos = [Philosopher(), Philosopher()]
+	map(lambda x: x.start() and x.join(), philos)
+
+test()
