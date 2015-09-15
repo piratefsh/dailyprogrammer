a004a90a4cda96e694a2b49c85c478ba8e9e113f
Sher Minn Chong <sherminn.chong@gmail.com>
2015-09-15 17:53:11 -0400

    Format and cleanup

--- misc/crypto/cc01.py
+++ misc/crypto/cc01.py
@@ -2,6 +2,7 @@ import binascii as ba
 import base64
 import codecs
 
+AVG_LEN_WORD = 5
 
 # challenge 1
 def hex2base64(data):
@@ -15,6 +16,7 @@ def xor(left, right):
 
 # challenge 3
 def decode_xor(encoded, key=None):
+    # if no key given, check for all possible keys from 1-127
     if key is None:
 
         # generate possible keys
@@ -25,22 +27,32 @@ def decode_xor(encoded, key=None):
         decoded_msgs = []
 
         for k in keys:
+            # get xor-ed string in hex
             hex_str = decode_xor(encoded, k)
+
+            # decode hex into ascii
             decoded = codecs.decode(hex_str, "hex")
 
+            # try to decode that into utf-8
             try:
                 decoded = decoded.decode('utf-8')
             except UnicodeDecodeError:
                 pass
 
-            if len([c for c in decoded if str(c).isalpha()]) > len(decoded)*0.6 and decoded.count(' ') > 1:
+            # is english if most of the characters are alphabets, and has at
+            # least half of spaces expected to be in sentence based off fact
+            # that average len of word is given
+            if len([c for c in decoded if str(c).isalpha()]) > len(decoded)*0.6 and decoded.count(' ') > (len(decoded)/AVG_LEN_WORD)/2:
                 decoded_msgs.append((ba.unhexlify(k), decoded))
 
         # return decoded messages
         return decoded_msgs
+
+    # else, check for just that key
     else:
         encoded_key = key * int(len(encoded)/len(key))
         return xor(encoded, encoded_key).zfill(len(encoded))
+
 
 def test():
     instr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
@@ -58,7 +70,6 @@ def test():
 
     msg = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
     msg_decoded = decode_xor(msg)
-    
-    for k,m in msg_decoded:
-        print(k, m)
 
+    for k, m in msg_decoded:
+        print(k, m)
