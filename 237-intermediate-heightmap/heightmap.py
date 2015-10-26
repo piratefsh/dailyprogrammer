e2896411732645c9c261c68cb7864dc8064340a4
Sher Minn Chong <sherminn.chong@gmail.com>
2015-10-26 15:06:49 -0400

    Add partial solution

--- 237-intermediate-heightmap/heightmap.py
+++ 237-intermediate-heightmap/heightmap.py
@@ -8,41 +8,61 @@ Boxes on layer 4 and above should not be filled.
 """
 char_map = ['#', '=', '-', '.', ' ']
 
+def rotate(boxes):
+    edge_map = {
+        '-' : '|',
+        '|' : '-',
+        ' ' : ' ',
+        '+' : '+'
+    }
+
+    rotated = []
+    for c, col in enumerate(boxes[0]):
+        rot_row = []
+        for r, row in enumerate(boxes):
+            rot_row.append(edge_map[row[c]])
+        rotated.append(rot_row)
+    return rotated
+
 def map_height(boxes):
     boxes = boxes.split('\n')
+    rotated = rotate(boxes)
 
     mapped = [[0] * len(row) for row in boxes]
 
     # count and keep number of edges on col
     num_edges = get_num_edges(boxes)
+
     curr_heights = [-1] * len(boxes[0])
     edge_count = [0] * len(boxes[0])
 
     for r, row in enumerate(boxes):
         for c, cell in enumerate(row):
-
             if cell == ' ':
-                # mapped[r][c] = char_map[curr_heights[c]]
-                mapped[r][c] = str(curr_heights[c])
+                mapped[r][c] = char_map[curr_heights[c]]
+                # mapped[r][c] = str(curr_heights[c])
             if cell == '-':
                 edge_count[c] += 1
                 if edge_count[c] <= num_edges[c]/2:
-                    curr_heights[c] = curr_heights[c] + 1 
+                    curr_heights[c] = curr_heights[c] + 1
                 else:
                     curr_heights[c] = curr_heights[c] - 1
+
                 mapped[r][c] = cell
-            elif cell == '|' or cell == '+':
+            
+            if cell == '|':
+                mapped[r][c] = cell
+
+            if cell == '+':
                 mapped[r][c] = cell
                 
-
-    #       if in box, draw symbol based off height
     return mapped
 
 def boxes_repr(boxes):
     outstr = ''
     for row in boxes:
         for col in row:
-            outstr += col
+            outstr += str(col)
         outstr += '\n'
     return outstr[:-1]
 
@@ -57,16 +77,24 @@ def get_num_edges(boxes):
 
     return num_edges
 
-def test():
+def get_inout(infile, outfile):
     boxes = None
     expected = None
 
-    # small input
-
-    with open('input01.txt') as f:
+    with open(infile) as f:
         boxes = f.read()
-    with open('output01.txt') as f:
+    with open(outfile) as f:
         expected = f.read() 
+    
+    return boxes, expected
+
+def write_out(outfile, content):
+    with open(outfile, 'w') as f: 
+        f.write(content)
+
+def test():
+    # small input
+    boxes, expected = get_inout('input01.txt', 'output01.txt')
 
     edges = get_num_edges(boxes.split('\n'))
     assert edges == [0, 2, 2, 4, 2, 2, 0]
@@ -75,17 +103,18 @@ def test():
     # assert boxes_repr(mapped) == expected
 
     # large input
-
-    with open('input00.txt') as f:
-        boxes = f.read()
-    with open('output00.txt') as f:
-        expected = f.read() 
-    
+    boxes, expected = get_inout('input02.txt', 'output02.txt')    
     mapped =  map_height(boxes)     
 
-    with open('output00-test.txt', 'w') as f: 
-        f.write(boxes_repr(mapped))
-
     assert boxes_repr(mapped) == expected
+    print('tests pass')
+
+def test_tricky():
+    # tricky
+    boxes, expected = get_inout('input00.txt', 'output00.txt') 
+    out = boxes_repr(map_height(boxes))
+    write_out('output00-test.txt', out)
+
+    assert out == expected
 
     print('tests pass')
